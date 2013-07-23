ca95ea746c7b54778b99d83666d5fdf78b35d51d
Piper Galgano <peg48@cornell.edu>
2013-07-23 12:25:49 -0400

    added year released validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
 	validates :title, :description, :year_released, presence: true
 	validates :description, length: { minimum: 10 }
+	validates :year_released, format: {with: /(19|20)\d{2}/i }
 
 	def self.search_for(query)
 		Movie.where("title LIKE :query OR description LIKE :query", {:query => "%#{query}%"})
